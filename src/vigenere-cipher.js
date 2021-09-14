f07100a54affeebcda10b9ec1411b8c1212fde4f
Alina <alinagusarik@gmail.com>
2021-09-14 22:49:08 +0300

    feat:task VigenereCipheringMachine complete

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,12 +20,66 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(direction=true) {
+    this.direct = direction
+    return this.direct
+ }
+  encrypt(message, key) {
+    if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    } else {
+      let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+      message = message.toUpperCase()
+      key = key.toUpperCase()
+      let code = []
+      let j = 0
+      for (let i = 0; i < message.length; i++) {
+      let item = abc[(abc.indexOf(message[i]) + abc.indexOf(key[j])) % 26]
+      if (abc.includes(message[i])){
+          code.push(item)
+          j++
+          if (j == key.length){ 
+          j = 0
+        }
+        } else {
+          code.push(message[i])
+        }
+      }
+     if (!this.direct) {
+        return code.reverse().join('')
+      } else {
+        return code.join('')
+      }
+    }
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(message, key) {
+     if (!message || !key) {
+      throw new Error('Incorrect arguments!')
+    } else {
+      let abc = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
+      message = message.toUpperCase()
+      key = key.toUpperCase()
+      let code = []
+       let j = 0
+       for (let i = 0; i < message.length; i++) {
+       let item = abc[(abc.indexOf(message[i]) - abc.indexOf(key[j])+26)% 26]
+         if (abc.includes(message[i])) {
+           code.push(item)
+           j++
+           if (j == key.length) {
+             j = 0
+           }
+         } else {
+           code.push(message[i])
+         }
+      }
+     if (!this.direct) {
+        return code.reverse().join('')
+      } else {
+        return code.join('')
+      }
+    }
   }
 }
