a54e68ed6cdac533afaf210a2dbd0fc1b67f092f
Alina <alinagusarik@gmail.com>
2021-09-15 23:02:53 +0300

    feat: task Simple-chain complete

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,38 @@ import { NotImplementedError } from '../extensions/index.js';
  * 
  */
 export default {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.chain.length
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  addLink(value) {
+    if (value===undefined) {
+    return  this.chain.push('( )') 
+    } else {
+      this.chain.push(`( ${value} )`)
+      return this
+    }
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  removeLink(position) {
+    if (typeof position === "number" && position > 0 && position < this.chain.  length ) {
+       this.chain.splice(position - 1, 1)
+      return this
+     
+    } else {
+      this.chain = []
+      throw new Error("You can't remove incorrect link!")
+   }
   },
+
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse()
+    return this
   },
+  
   finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    let result = this.chain.join('~~')
+    this.chain=[]
+     return result
   }
 };
