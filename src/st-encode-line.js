0754f6d29676b6db3ef04f544919fd29148f6ef5
Alina <alinagusarik@gmail.com>
2021-09-22 00:11:51 +0300

    feat:task st-encode-line complete

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,22 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+  str=str.split('')
+  console.log(str)
+  let stack = [str[0]]
+  let result=[]
+  for (let i = 1; i < str.length; i++){
+    if(stack[stack.length - 1] === str[i]) {
+      stack.push(str[i])
+    }
+    else {
+      result.push(stack.length)
+      result.push(stack[stack.length - 1])
+      stack = [str[i]]
+      
+    }
+  }  result.push(stack.length)
+      result.push(stack[stack.length - 1])
+  return result.join('').split('1').join('')
 }
--- src/transform-array.js
+++ src/transform-array.js
@@ -28,28 +28,22 @@ if ( arr[arr.length - 1] === instruction[0] || arr[arr.length - 1] === instructi
        return  arr.slice(0,-1)
       }
     
-    for (let i = 0; i < arr.length; i++) {
-        
-      if (arr[i] === '--double-next') {
-          stack.push(arr[i + 1])
-      }
-      else if (arr[i] === '--discard-prev') {
-          stack.pop()     
-      }
-      else if (arr[i] === '--discard-next') {
-         if (arr[i + 2] === '--discard-prev') {
-          stack.push(arr[i + 1])
-        }
-        i++
-     } else if (arr[i] === '--double-prev') {
-       if (arr[i - 2] !== '--discard-next') {
-          stack.push(arr[i - 1])
-        }
-      } else {
-        stack.push(arr[i])
-      }
-      
-    } return stack
+     for (let i = 0; i < arr.length; i++) {
+
+            if (arr[i] === '--double-next') {
+                stack.push(arr[i + 1])
+            } else if (arr[i] === '--discard-prev' && arr[i - 2] !== '--discard-next') {
+                stack.pop()
+            } else if (arr[i] === '--discard-next') {
+                i++
+            } else if (arr[i] === '--double-prev' && arr[i - 2] !== '--discard-next') {
+                stack.push(arr[i - 1])
+            } else if (!instruction.includes(arr[i])) {
+                stack.push(arr[i])
+            }
+
+    
+  } return stack
   } else {throw new Error("'arr' parameter must be an instance of the Array!")
   }
 }
